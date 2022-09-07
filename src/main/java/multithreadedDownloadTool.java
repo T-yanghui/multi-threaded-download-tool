5ee9622f3265683f98380b98bc0f18585ed148ac
yang <CLRZBJ@163.com>
2022-09-07 17:15:46 +0800

    born

---
+++ src/main/java/multithreadedDownloadTool.java
@@ -0,0 +1,9 @@
+/**
+ * @author yang
+ * @version 1.0.0
+ * @ClassName multithreadedDownloadTool.java
+ * @Description TODO
+ * @createTime 2022/09/07
+ */
+public class multithreadedDownloadTool {
+}
