097a9eec902de4236a2d347b28d250d13a1d93f7
yang <CLRZBJ@163.com>
2022-09-08 18:44:16 +0800

    finish the most function

--- src/main/java/MultithreadedDownloadTool.java
+++ src/main/java/MultithreadedDownloadTool.java
@@ -3,8 +3,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author yang
@@ -15,34 +18,37 @@ import java.util.concurrent.RecursiveAction;
  */
 public class MultithreadedDownloadTool {
     private static long fileSize;
-    private static final long splitThreshold=1L*1024L*1024L;
-    private static RandomAccessFile randomAccessFile;
+    private static final long splitThreshold=6*1024*1024L;
+    private static File file;
+    private static ThreadLocal<RandomAccessFile> randomAccessFileThreadLocal=new ThreadLocal<>();
     private static String requestURL;
     private static int threadNum;
+    private static AtomicInteger count;
     private static final String RANGEFORMATE="bytes=%s-%s";
     private static final String USERAGENT="Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0";
     private static final String FILE_ACCESS_MODE="rwd";
     public MultithreadedDownloadTool(String requestURL,String storePath,int threadNum) throws IOException {
         this.requestURL=requestURL.trim();
-        File file=new File(storePath.trim());
-        this.randomAccessFile=new RandomAccessFile(file,FILE_ACCESS_MODE);
-        this.fileSize=HttpUtils.getHttpConnection(new Properties()).getContentLength();
-        System.out.println(fileSize);
-        this.randomAccessFile.setLength(fileSize);
+        this.file=new File(storePath.trim());
+        this.fileSize=HttpUtils.getHttpConnection(new Properties()).getContentLengthLong();
         this.threadNum=threadNum;
+        this.count=new AtomicInteger(0);
     }
-    public void downLoad() throws IOException {
+    public void downLoad() throws IOException, InterruptedException {
         int availableThread=Runtime.getRuntime().availableProcessors();
         ForkJoinPool forkJoinPool=new ForkJoinPool(availableThread<threadNum?availableThread:threadNum);
         forkJoinPool.submit(new workers((int)fileSize));
         forkJoinPool.shutdown();
-        randomAccessFile.close();
+        forkJoinPool.awaitTermination(1, TimeUnit.DAYS);
     }
     public long getFileSize(){
         return fileSize;
     }
-    public RandomAccessFile getRandomAccessFile(){
-        return randomAccessFile;
+    public int getProcess(){
+        return (int)(count.get()/fileSize);
+    }
+    public File getFile(){
+        return this.file;
     }
     public static class HttpUtils {
         public static HttpURLConnection getHttpConnection(Properties properties) throws IOException {
@@ -74,40 +80,72 @@ public class MultithreadedDownloadTool {
     private static class workers extends RecursiveAction{
         private int start;
         private int end;
-        private int count;
-        public workers(int end){
-           this(0,end);
+        public workers(int end) throws FileNotFoundException {
+            this(0,end);
         }
-        public workers(int start,int end){
+        public workers(int start,int end) throws FileNotFoundException {
             this.start=start;
             this.end=end;
-            this.count=0;
         }
         @Override
         protected void compute() {
-           if(end-start<=splitThreshold){
-               try {
-                   Properties properties = new Properties(2);
-                   properties.put("User-Agent", USERAGENT);
-                   properties.put("Range", String.format(RANGEFORMATE, start, end));
-                   HttpURLConnection httpURLConnection = HttpUtils.getHttpConnection(properties);
-                   byte[] data= HttpUtils.getDataByte(httpURLConnection);
-                   randomAccessFile.write(data,start, data.length);
-               }catch (Exception e){
-                   System.out.println("something wrong...");
-                   e.printStackTrace();
-               }
-           }else{
-               int mid=start+(end-start>>1);
-               workers worker1=new workers(start,mid);
-               workers worker2=new workers(mid+1,end);
-               worker1.fork();
-               worker2.fork();
-           }
+            if(end-start<=splitThreshold){
+                try {
+                    Properties properties = new Properties(2);
+                    properties.put("User-Agent", USERAGENT);
+                    properties.put("Range", String.format(RANGEFORMATE, start, end));
+                    HttpURLConnection httpURLConnection = HttpUtils.getHttpConnection(properties);
+                    byte[] data= HttpUtils.getDataByte(httpURLConnection);
+                    if(null==randomAccessFileThreadLocal.get()){
+                        randomAccessFileThreadLocal.set(new RandomAccessFile(file,FILE_ACCESS_MODE));
+//                        System.out.println("randomFile is null...fix it"+"***"+Thread.currentThread().getId());
+                    }
+                    randomAccessFileThreadLocal.get().seek(start);
+                    randomAccessFileThreadLocal.get().write(data,0, data.length);
+                    count.addAndGet(data.length);
+                }catch (Exception e){
+                    System.out.println("something wrong...");
+                    e.printStackTrace();
+                }
+            }else{
+                try {
+                    int mid = start + (end - start >> 1);
+                    workers worker1 = new workers(start, mid);
+                    workers worker2 = new workers(mid, end);
+                    invokeAll(worker1, worker2);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
+    static class ProgressBar extends Thread {
+        private static final String printFormat = "progress: %.2f %%";
+        /**
+         * 打印进度条（长度变化）
+         * @throws InterruptedException 抛出异常
+         */
+        static void printProgressBar(double percent) throws InterruptedException {
+            percent*=100;
+            String out=String.format(printFormat,percent);
+            System.out.print(out);
+            Thread.sleep(500);
+            System.out.print("\b".repeat(out.length()));
+        }
 
-    public static void main(String[] args) throws IOException {
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    printProgressBar( 1.0*count.get() / fileSize);
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner in=new Scanner(System.in);
         String url;
         String path;
@@ -119,7 +157,14 @@ public class MultithreadedDownloadTool {
         System.out.println("threadNum:");
         threadNum=in.nextInt();
         in.close();
+        long startTime=System.currentTimeMillis();
         MultithreadedDownloadTool multithreadedDownloadTool=new MultithreadedDownloadTool(url,path,threadNum);
+        Thread daemendThread=new Thread(new ProgressBar());
+        daemendThread.setDaemon(true);
+        daemendThread.start();
         multithreadedDownloadTool.downLoad();
+        long endTime=System.currentTimeMillis();
+        System.out.printf("\nfinished... total time: %d s",(endTime-startTime)/1000);
+        System.out.println("\nfile path: "+path);
     }
 }
--- src/test/java/MultithreadedDownloadToolTest.java
+++ src/test/java/MultithreadedDownloadToolTest.java
@@ -1,11 +1,10 @@
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yang
@@ -16,30 +15,26 @@ import java.util.Properties;
  */
 public class MultithreadedDownloadToolTest {
     private Properties properties;
-    private static final String URL="http://download.manjaro.org/kde/21.3.7/manjaro-kde-21.3.7-220816-linux515.iso";
+    private static final String requstURL="https://publish.u-tools.cn/version2/uTools-2.1.0.exe";
     private static final String RANGEFORMATE="bytes=%s-%s";
     private MultithreadedDownloadTool multithreadedDownloadTool;
     @Before
     public void setUp() throws Exception{
-        //multithreadedDownloadTool=new MultithreadedDownloadTool(URL,"/home/yang/picTest",1);
+        multithreadedDownloadTool=new MultithreadedDownloadTool(requstURL,"/home/yang/picTest2",10);
         properties=new Properties();
         properties.put("User-Agent","Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0");
-//        properties.put("Range",String.format(RANGEFORMATE,0,multithreadedDownloadTool.getFileSize()));
+        properties.put("Range",String.format(RANGEFORMATE,0,multithreadedDownloadTool.getFileSize()));
     }
 
     @Test
-    public void downLoadTest() {
+    public void downLoadTest() throws IOException, InterruptedException {
+        multithreadedDownloadTool.downLoad();
     }
     @Test
-    public void getHttpConnectionTest() throws IOException {
-        HttpURLConnection conn=MultithreadedDownloadTool.HttpUtils.getHttpConnection(properties);
-        System.out.println(conn.getContentLength());
-        System.out.println(conn.getContent());
-//        System.out.println(multithreadedDownloadTool.getFileSize());
-//        byte[] data=MultithreadedDownloadTool.HttpUtils.getDataByte(conn);
-//        System.out.println(data.length);
-//        RandomAccessFile randomAccessFile=multithreadedDownloadTool.getRandomAccessFile();
-//        randomAccessFile.write(data,0,data.length);
-//        randomAccessFile.close();
+    public void getHttpConnectionTest() throws IOException, InterruptedException {
+    }
+    @Test
+    public void processBarTest() throws InterruptedException {
+        MultithreadedDownloadTool.ProgressBar.printProgressBar(0.123455);
     }
 }
